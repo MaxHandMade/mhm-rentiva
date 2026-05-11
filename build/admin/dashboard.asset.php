80d85763d8515154e762886b26e650b3d437a775
MaxHandMade <maxhandmadeonline@gmail.com>
2026-05-11 20:36:52 +0300

    fix(mobile): stack dashboard widget rows to single column at 782px breakpoint

--- build/admin/dashboard.asset.php
+++ build/admin/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cee9957c64619034121c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a06983edd038fe1b296d');
