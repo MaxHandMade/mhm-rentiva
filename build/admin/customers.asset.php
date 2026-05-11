131886f1a756204d414285c2f5ac041636b0af31
MaxHandMade <maxhandmadeonline@gmail.com>
2026-05-11 11:52:52 +0300

    build(customers): update build artifacts

--- build/admin/customers.asset.php
+++ build/admin/customers.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '01a56a21d813c8f1a225');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a89dc673fd43212e9e7a');
