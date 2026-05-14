ed917b79ef62a513d93c2344092083ce0bf51df3
MaxHandMade <maxhandmadeonline@gmail.com>
2026-05-14 14:41:05 +0300

    feat: add Transfer and Vendors buttons to dashboard quick actions

--- build/admin/dashboard.asset.php
+++ build/admin/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '382b133ed18e29cae1fc');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3b28570385222200633a');
