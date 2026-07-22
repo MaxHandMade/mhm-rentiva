59e070d407b2fd649fbeb0de831530c057e89907
MaxHandMade <maxhandmadeonline@gmail.com>
2026-07-22 19:32:39 +0300

    style(dashboard): scope stat-card delta selectors under #id

--- build/admin/dashboard.asset.php
+++ build/admin/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '087f2d4d64e5283ce6a6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9e60c8c812740586141f');
